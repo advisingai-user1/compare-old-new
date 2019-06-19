625074dfe0902fcfecf9ea31b81c092fe4fb5cf0
Charles <momin@advisingai.com>
2019-06-19 10:54:34 -0500

    inital

---
+++ compare.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def same(old, new):
+    return false
+
+def compare():
+    return "json"
+
+
+def main():
+    return 0
+
+if __name__ == '__main__':
+    main()
+    
+
